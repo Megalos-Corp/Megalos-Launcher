3dd1ca78625cd7bf755eb2afd8fe7d65bab20ad3
Nesios <52352139+N3siOS@user.noreply.github.com>
2022-05-23 13:25:17 +0200

    Change connexion protocole

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { app, BrowserWindow, ipcMain } = require("electron");
+const { Authenticator } = require("minecraft-launcher-core");
 const path = require("path");
 
 app.whenReady().then(() => createWindow());
@@ -17,12 +18,20 @@ function createWindow() {
 
     mainWindow.loadURL(path.join(__dirname, "index.html"));
 }
-app.on("activate", () => {
-    if (mainWindow == null) {
-        createWindow();
-    }
-});
 
-app.on("window-all-closed", () => {
-    app.quit();
+app.whenReady().then(() => {
+    createWindow();
+
+    app.on("activate",function () {
+        if (process.plateform !== "darwin") app.quit();
+    });
+  ipcMain.on("login", (evt, data) => {
+      Authenticator.getAuth(data.user, data.pass)
+      .then((e) => {
+          mainWindow.loadURL(path.join(__dirname, "app.html"));
+      })
+      .catch(() => {
+          evt.sender.send("err", "Erreur de connexion");
+      });
+  })  
 })
